835e3aabbbe78e6b6576ed40ea203513e4b1904a
AndyP271838 <P271838@tafe.wa.edu.au>
2022-07-22 20:49:20 +0800

    LoadData Method
    Began Creation of LoadData and Show all methods

--- DataProcessing/MainWindow.xaml.cs
+++ DataProcessing/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Galileo6;
 
 namespace DataProcessing
 {
@@ -27,13 +28,42 @@ namespace DataProcessing
         public MainWindow()
         {
             InitializeComponent();
+            upDownMu.Text = "50";
+            upDownSig.Text = "10";
         }
 
-        //Load Data Method, po
+        //Load Data Method, return void no solution, uses loop to populate linked list to size not greater than 400
 
-        public static void LoadData()
+        public void LoadData()
         {
+            ReadData dataReader = new ReadData();
+            for (int i =0; i < 400; i++)
+            {
+                SensorA.AddFirst(dataReader.SensorA(Double.Parse(upDownMu.Text), Double.Parse(upDownSig.Text)));
+                SensorB.AddFirst(dataReader.SensorB(Double.Parse(upDownMu.Text),Double.Parse(upDownSig.Text)));
+            }
+            
+        }
 
+        //Show all sensors method, display both LinkedLists in a ListView
+        public void ShowAllSensorData()
+        {
+            lstViewStaticDisplay.Items.Add(item);
+            //ListViewItem
+            //lstViewStaticDisplay.
+            //for (int i = 0; i < SensorA.Count; i++)
+            //{
+            //    ListViewItem item = new ListViewItem();
+            //    item.Content = SensorA;
+            //    lstViewStaticDisplay.Items.Add(new ListViewItem());
+            //}
+        }
+
+        //Call LoadData method, and ShowAllSensorData methods, no parameters
+        private void btnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+            ShowAllSensorData();
         }
     }
 }
